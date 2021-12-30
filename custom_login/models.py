d2bb5badae804e361144a9e44c4e240a2e2f7322
rezacs <ralmassizx@gmail.com>
2021-12-30 23:41:42 +0330

    adding custom user

---
+++ custom_login/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+from custom_login.myusermanager import *
+
+
+class MyUser ( AbstractUser ) :
+    username = None
+    mobile = models.CharField(max_length=11 , unique=True)
+    otp = models.PositiveIntegerField(blank=True , null=True)
+    otp_created_time = models.DateTimeField(auto_now=True)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'mobile'
+
+    REQUIRED_FIELDS = []
+
+    backend = ''
