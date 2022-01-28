a0323a461c5283742661d2b365995a5df63e842f
rezacs <ralmassizx@gmail.com>
2022-01-28 23:20:38 +0330

    ss

--- homew/settings.py
+++ homew/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*rf*grz$bx@9!*2(swzi^r*3xh+2cr=d76rc$(7nqb$z=mea_z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['bi3z.com' , 'www.bi3z.com']
 
 
 # Application definition
